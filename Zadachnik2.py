fa271bb98c5652d89a4613650c7f206e0a023f8e
Anton <Burhanov08@mail.ru>
2021-06-02 21:56:51 +0400

    1.Привел текст в соответсвии требованиям руководству PEP8. Есть мнение что необходжимо использовать наследование имен в функциях, но не совсем понимаю как это работает. 2. вынес основной цикл в отдельную функцию под названием _osn_code, с использованием конструкции неполного условия if и специальной переменной __name__ которой присваивается значение __main__, так-как мой исходный код запустился как основная программа. Если бы основной код запускался бы из другой исходного файла то значение переменной __name__ имелдо бы названием этого модуля (исходного файла.  [#1]

--- Zadachnik2.py
+++ Zadachnik2.py
@@ -1,18 +1,16 @@
 import json
 
-# with open("zadachi.json", "w") as fl:
-# data = json.load(fl)
-data = None
+_data = None
 
 
 def opn():
-    global data
+    global _data
     with open("zadachi.json", "r",encoding='utf-8') as fl:
-        data = json.load(fl)
-        for i in data:
+        _data = json.load(fl)
+        for i in _data:
             print(f"№{i['id']} {i['title']}")
     nt = int (input())
-    for i in data:
+    for i in _data:
         if nt == i['id']:
             for j in i['tasks']:
                 print(j['task'])
@@ -21,16 +19,11 @@ def opn():
                     print ('Верно')
                 else:
                     print('Не верно')
-
-
-
+# Функция вывода задания на экран с проверкой ответов
 
 def rd():
     with open("zadachi.json", "r", encoding='utf-8') as fl:
-        data = json.load(fl)
-
-
-
+        _data = json.load(fl)
     while True:
         new_data = {
             "title": '',
@@ -41,8 +34,7 @@ def rd():
         if tnew == '0':
             break
         new_data["title"] = tnew
-        new_data["id"] = get_last_id (data) + 1
-
+        new_data["id"] = get_last_id (_data) + 1
         new_t_id = 1
         while True:
             new_tasks = {
@@ -59,48 +51,50 @@ def rd():
             new_tasks["id"] = new_t_id
             new_t_id = new_t_id+1
             new_data['tasks'].append(new_tasks)
-        data.append(new_data)
+        _data.append(new_data)
     with open("zadachi.json", "w", encoding='utf-8') as fl:
-        json.dump(data,fl,indent=4)
+        json.dump(_data,fl,indent=4)
+# Функция записи нового задания
 
-
-def get_last_id (data):
+def get_last_id (_data):
     n_id = 0
-    for i in data:
+    for i in _data:
         if i['id'] > n_id:
             n_id = i['id']
     return n_id
+# Подфункция определения последнего номера задачи функции записи новыз задач
 
+
+def _osn_code():
+    while True:
+        comand = input('Введите номер команды \n')
+        if comand.lower() == 'help':
+            print('Help - вызов списка комманд \n 1- Приступить к решенипю задач '
+                  '\n New - Добавить собственное задание в задачник \n  Info - информация '
+                  'о задачнике\n Exit - выход'
+                  )
+        elif comand == '1':
+            print('выбери номер темы:')
+            opn()
+        elif comand == 'new':
+            rd()
+        elif comand.lower() == 'info':
+            print('Данный задачник является тестовым заданием для обучения программирования'
+                  ' на языке Python,\n все найденные недочеты и зачечания по данному прилож'
+                  'ению можно отправить по\n електронной почте burhanov08@mail.ru'
+                  )
+        elif comand.lower() == 'exit':
+            print('До новых встреч!', name)
+            exit()
+        elif comand == '':
+            print(name, 'Вы ничего не ввели')
+        else:
+            print(name, '- Вы ввели неизвенстную мне комманду! Или допустили опечатку попробуйте еще раз')
 
 
 print('Привет, как тебя зовут?')
-Name = input()
-print(Name, 'Вы открыли сборник задач по информатике')
+name = input()
+print(name, 'Вы открыли сборник задач по информатике')
 print('Для вызова списка комманд введи Help, или укажи известную тебе комманду')
-
-while True:
-    comand = input('Введите номер команды \n')
-    if comand.lower() == 'help':
-        print(
-            'Help - вызов списка комманд \n 1- Приступить к решенипю задач \n New - Добавить собственное задание в задачник \n  Info - информация о задачнике\n Exit - выход')
-        #comand = input()
-    elif comand == '1':
-        print ('выбери номер темы:')
-        opn()
-    elif comand == 'new':
-        rd()
-    # elif comand == '3':
-
-    elif comand.lower() == 'info':
-        print(
-            'Данный задачник является тестовым заданием для обучения программирования на языке Python,\n все найденные недочеты и зачечания по данному приложению можно отправить по\n електронной почте burhanov08@mail.ru')
-        #comand = input()
-    elif comand.lower() == 'exit':
-        print('До новых встреч!', Name)
-        exit()
-    elif comand == '':
-        print(Name, 'Вы ничего не ввели')
-        #comand = input()
-    else:
-        print(Name, '- Вы ввели неизвенстную мне комманду! Или допустили опечатку попробуйте еще раз')
-       # comand = input()
+if __name__ == "__main__":
+    _osn_code ()
